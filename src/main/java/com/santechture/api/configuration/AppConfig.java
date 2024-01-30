54c04d2cffd3d64967574c59a290fbf8416b1524
r-mo7amed <126299299+r-mo7amed@users.noreply.github.com>
2024-01-30 06:06:12 +0200

    first commit
    first commit

---
+++ src/main/java/com/santechture/api/configuration/AppConfig.java
@@ -0,0 +1,30 @@
+package com.santechture.api.configuration;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import util.JwtUtil;
+
+@Component
+public class AppConfig {
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setBasenames("lang/messages");
+        return messageSource;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean validator(MessageSource messageSource) {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource);
+        return bean;
+    }
+
+}
---
+++ src/main/java/com/santechture/api/service/AdminService.java
@@ -0,0 +1,36 @@
+package com.santechture.api.service;
+
+import com.santechture.api.dto.GeneralResponse;
+import com.santechture.api.dto.admin.AdminDto;
+import com.santechture.api.entity.Admin;
+import com.santechture.api.exception.BusinessExceptions;
+import com.santechture.api.repository.AdminRepository;
+import com.santechture.api.validation.LoginRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+public class AdminService {
+
+
+    private final AdminRepository adminRepository;
+
+
+    public AdminService(AdminRepository adminRepository) {
+        this.adminRepository = adminRepository;
+    }
+
+    public ResponseEntity<GeneralResponse> login(LoginRequest request) throws BusinessExceptions {
+
+        Admin admin = adminRepository.findByUsernameIgnoreCase(request.getUsername());
+
+        if(Objects.isNull(admin) || !admin.getPassword().equals(request.getPassword())){
+            throw new BusinessExceptions("login.credentials.not.match");
+        }
+
+        return new GeneralResponse().response(new AdminDto(admin));
+    }
+}
